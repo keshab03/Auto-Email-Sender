3549f6c0e804acc8214a7a38c50e519e3182eb73
unknown <keshabmahanta03@gmail.com>
2025-01-23 19:27:37 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const port = 5500;
-export const url = 'http://localhost:5500';
+// export const url = 'http://localhost:5500';
+export const url = 'https://auto-email-sender-6rsh.onrender.com';
